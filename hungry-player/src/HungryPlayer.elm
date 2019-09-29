1276e194a148f686c444f081603a52dd3d945bfa
Oliver Bucher <level8broccoli@gmail.com>
2019-09-29 22:10:03 +0200

    first step with elm

--- hungry-player/src/HungryPlayer.elm
+++ hungry-player/src/HungryPlayer.elm
@@ -1,7 +1,47 @@
 module HungryPlayer exposing (main)
 
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (..)
 
 
+playerInfo : String -> String -> String -> String
+playerInfo name orders paymentoptions =
+    name
+        ++ " hat bestellt: "
+        ++ orders
+        ++ " und bezahlt mit "
+        ++ paymentoptions
+
+
+viewPlayerInfo : Html msg
+viewPlayerInfo =
+    let
+        playerText =
+            playerInfo "Oliver" "Hawaii" "Resolut"
+                |> text
+    in
+    h2 [] [ playerText ]
+
+
+viewHeader : Html msg
+viewHeader =
+    h1 [] [ text "HungryPlayers" ]
+
+
+viewFooter : String -> Html msg
+viewFooter input =
+    footer [] [ text input ]
+
+
+view : Html msg
+view =
+    main_ []
+        [ viewHeader
+        , viewPlayerInfo
+        , viewFooter "Footerly"
+        ]
+
+
+main : Html msg
 main =
-    Html.text "Helloo"
+    view
